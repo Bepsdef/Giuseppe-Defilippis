00430ad2588e969e4be81e247df516c8f374b993
Bepsdef <giuseppedefilippis23@gmail.com>
2022-07-31 09:22:46 +0200

    dsad

---
+++ main.js
@@ -0,0 +1,10 @@
+// main.js
+
+  $(document).ready(function() {
+    $(document).on('mousemove', function(e) {
+      $('#circularcursor').css({
+        left: e.pageX,
+        top: e.pageY
+      });
+    })
+  });
