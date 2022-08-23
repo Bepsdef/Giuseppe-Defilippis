9536aafcd83ba357f4d3319d8085def3d375cfaf
Bepsdef <giuseppedefilippis23@gmail.com>
2022-08-23 19:29:31 +0200

    fuuuck

--- main.js
+++ main.js
@@ -1,10 +1,50 @@
 // main.js
 
-  $(document).ready(function() {
-    $(document).on('mousemove', function(e) {
-      $('#circularcursor').css({
-        left: e.pageX,
-        top: e.pageY
-      });
-    })
+
+
+  const btn1 = document.getElementById('btn1');
+
+
+  btn1.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+   const totaleditor = document.getElementById('totaleditor');
+   totaleditor.style.backgroundColor = 'white';
+  
+    // 👇️ optionally change text color
+   editorbg.style.color = 'black';
   });
+
+  
+  const btn2 = document.getElementById('btn2');
+
+
+  btn2.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+   const totaleditor = document.getElementById('totaleditor');
+   totaleditor.style.backgroundColor = 'black';
+  
+    // 👇️ optionally change text color
+   editorbg.style.color = 'white';
+  });
+
+
+
+  const btn3 = document.getElementById('btn3');
+
+
+  btn3.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+   const totaleditor = document.getElementById('totaleditor');
+   totaleditor.style.backgroundColor = '#ffe201'
+  
+    // 👇️ optionally change text color
+   editorbg.style.color = 'black';
+  });
+
+
+
+
+
+
+
+  
